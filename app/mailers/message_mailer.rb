7a9822135a67ef72bc579fa72255cf3fc151944f
juliotoscano <jjortb@cin.ufpe.br>
2017-11-29 16:57:15 +0000

    meila corigido

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,9 +1,8 @@
 class MessageMailer < ApplicationMailer
     default from: ENV["SECRET_EMAIL_BASE"]
-    
     def new_message(message)
         @message = message
-        mail(:to => @message.email, :subject => "Alguem entrou em contato!") do |format|
+        mail(:to => ENV["SECRET_EMAIL_BASE"], :reply_to => @message.email , :subject => "Usuario entrou em contato: #{@message.email}") do |format|
             format.text {render 'message_mailer/mailer'}
             format.html {render 'message_mailer/new_message'}
         end
